b27aca758e3fce2763286fa8c985f4421409a4bd
DennisADLL <dennismoralesic@email.com>
2026-05-24 22:32:19 -0500

    NUEVO ADDIN - DUPLICAR PLANOS CON VISTAS

---
+++ AddCreatePlansSelectView.cs
@@ -0,0 +1,58 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace add
+{
+    [Transaction(TransactionMode.Manual)]
+    public class AddCreatePlansSelectView : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<ElementId> elementsIdSelection = uidoc.Selection.GetElementIds().ToList();                     
+
+            //List<View> elementViews = elementsIdSelection.Select(r => doc.GetElement(r)).OfType<View>().ToList();
+
+            List<ViewSheet> elementViewSheet = elementsIdSelection.Select(r => doc.GetElement(r)).OfType<ViewSheet>().ToList();
+
+            TaskDialog.Show("Views", elementViewSheet.Count.ToString());
+
+            foreach (ViewSheet viewSheet in elementViewSheet)
+            {
+                using (Transaction tran = new Transaction(doc, "Create plans of views"))
+                {
+                    tran.Start();
+
+                    ElementId duplicatedViewSheetId = viewSheet.Duplicate(SheetDuplicateOption.DuplicateSheetWithViewsOnly);
+
+                    ViewSheet elementoViewSheetDuplicated = doc.GetElement(duplicatedViewSheetId) as ViewSheet;
+
+                    tran.Commit();
+                }
+            }
+                       
+
+            return Result.Succeeded;
+        }
+
+        public static double MnToFt(int mm)
+        {
+            return UnitUtils.ConvertToInternalUnits(mm, UnitTypeId.Millimeters);
+        }
+    }
+
+    
+}
---
+++ ConstructorPersona.cs
@@ -0,0 +1,37 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace add
+{
+    // CLASE PARA CREAR OBJETOS DE TIPO PERSONA, CON UN CONSTRUCTOR QUE RECIBE UN PARAMETRO DE TIPO STRING, Y ESTO ES CAPTURADO POR EL ATRIBUTO "NOMBRE"
+    class ConstructorPersona
+    {
+        public string Nombre;
+        public int Edad;
+
+        public ConstructorPersona(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+
+    }
+    
+    //INCIO DE LA APLICACION
+    class Program
+    {
+        static void Main()
+        {
+            ConstructorPersona p = new ConstructorPersona("Juan", 28); // SE INTANCIA EL CONSTRUCTOR (SE CREA EL OBJETO),
+                                                                       // los inputs son capturados por el constructor y asignados como valor a los atributos de la clase
+
+            MessageBox.Show(p.Nombre + p.Edad); // ACCESO A LOS ATRIBUTOS DE LA CLASE (MESSAGEBOX Y CONSOLE.WRITELINE SOLO FUNCIONAN CON CONSOLE PROYECTO, NO CON .NET FRAMEWORK)
+        }
+    }
+}
